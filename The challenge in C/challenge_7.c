77f3973d208dd6cfc49408001de96fdd78c3d831
jamalraouj <jamalrwj@gmail.com>
2021-11-11 11:41:06 +0100

    add challenge 7-6

---
+++ The challenge in C/challenge_7.c
@@ -0,0 +1,12 @@
+
+int main() {
+	
+	int a,b;
+	printf("entre A :");
+    	scanf("%d", &a);
+    printf("entre B :");
+    	scanf("%d", &b);
+    	
+    	printf("a + b = %d \n a - b = %d \n a * b = %d \n a / b = %d \n a % b = %d",a+b ,a-b ,a*b ,a/b,a%b);
+	return 0;
+}
